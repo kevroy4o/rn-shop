b4369f0f7003ee9f048c197d94f3f91ddc76aa6c
Kevin Richardson <richalk@gmail.com>
2020-02-10 14:38:29 +0000

    Setup main folder structure and redux store

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+	constructor(id, ownerId, title, imageUrl, description, price) {
+		this.id = id;
+		this.title = title;
+		this.ownderId = ownerId;
+		this.imageUrl = imageUrl;
+		this.description = description;
+		this.price = price;
+	}
+}
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Flatlist } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+	return <Flatlist />;
+};
+
+export default ProductsOverviewScreen;
